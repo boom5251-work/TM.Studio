886d023cec389a7734076cc079a8161854930053
boom5251-work <boom5251@list.ru>
2022-09-23 00:37:35 +0300

    Добавьте файлы проекта.

---
+++ TransportManagement.Studio/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TransportManagement.Studio
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
